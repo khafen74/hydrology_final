e230063ff314ebbe7f74cbba1c2d348cccc559c3
Konrad Hafen <khafen74@gmail.com>
2015-12-14 20:31:52 -0700

    playing games

--- main.py
+++ main.py
@@ -14,14 +14,17 @@ dep_path2 = r'C:\Users\konrad\Desktop\Classes\CEE_6400_Hydrology\FinalProject\Da
 
 df_new = resample_timeseries.getDf(filename)
 df_start = df_new
-df_new['No Ponds'] = pond_rasters.calcET(df_start, freq_path, dep_path, False)
-df_new['Ponds Actual'] = pond_rasters.calcET(df_start, freq_path, dep_path, False)
-df_new['Ponds 50% BRAT'] = pond_rasters.calcET(df_start, freq_path, dep_path1, False)
-df_new['Ponds 100% BRAT'] = pond_rasters.calcET(df_start, freq_path, dep_path2, False)
+df_new['No Ponds'] = pond_rasters.calcET(df_start, freq_path, dep_path, 2)
+df_new['Ponds Actual'] = pond_rasters.calcET(df_start, freq_path, dep_path, 3)
+df_new['Ponds 50% BRAT'] = pond_rasters.calcET(df_start, freq_path, dep_path1, 3)
+df_new['Ponds 100% BRAT'] = pond_rasters.calcET(df_start, freq_path, dep_path2, 3)
 
-df_new.drop(df_new.columns[[0,1,2]], axis=1, inplace=True)
+df_new.drop(df_new.columns[[0,1,2,3]], axis=1, inplace=True)
+
+#print df_new
 
 df_new = df_new.cumsum()
+
 
 plt.figure; df_new.plot();
 plt.show()
--- pond_rasters.py
+++ pond_rasters.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 from osgeo import gdal
 
-def calcET(df_et, freq_path, dep_path, pond=True):
+def calcET(df_et, freq_path, dep_path, pond):
     freqDS = gdal.Open(freq_path, gdal.GA_ReadOnly)
     depDS = gdal.Open(dep_path, gdal.GA_ReadOnly)
     freq_band = freqDS.GetRasterBand(1)
@@ -27,16 +27,26 @@ def calcET(df_et, freq_path, dep_path, pond=True):
     et_list = df_et.values.tolist()
     et_noPond = []
     et_pond = []
+    et_diff = []
 
     for i in et_list:
         noPond = (nDry * i[1]) * cellArea_mm2 * toM3
-        pond = (((nDry - nWet) * i[1] * cellArea_mm2) + ((nWet * i[1] * 1.05) * cellArea_mm2)) * toM3
+        pondVal = (((nDry - nWet) * i[1] * cellArea_mm2) + ((nWet * i[1] * 1.05) * cellArea_mm2)) * toM3
 
         et_noPond.append(noPond)
-        et_pond.append(pond)
+        et_pond.append(pondVal)
+        et_diff.append(pondVal-noPond)
 
-    if pond:
+    print pond
+    if pond == 1:
+        print 'returning pond'
         return et_pond
-    else:
+    elif pond == 2:
+        print 'returning no pond'
         return et_noPond
+    elif pond == 3:
+        print 'returning et_diff'
+        return et_diff
+    else:
+        print 'not returning anything'
 
